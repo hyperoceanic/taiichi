50e1a0771049574c8153857f0e1009a1d1d150ea
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-01-24 12:44:28 +0000

    first

---
+++ test/taiichi_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule TaiichiWeb.ErrorJSONTest do
+  use TaiichiWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert TaiichiWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert TaiichiWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
