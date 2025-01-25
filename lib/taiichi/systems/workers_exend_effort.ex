a23e996ab400d94e7dcecff457e3c203937b10a6
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-01-25 00:58:03 +0000

    Tidy up console output

--- lib/taiichi/systems/workers_exend_effort.ex
+++ lib/taiichi/systems/workers_exend_effort.ex
@@ -20,17 +20,19 @@ defmodule Taiichi.Systems.WorkersExendEffort do
 
         assignment_name = HasAName.get(assignment_id)
 
-        IO.puts "#{worker_name} is assigned to assignment named '#{assignment_name}'"
-
         task_id = TaskAssignment.get(assignment_id)
 
         task_name = HasAName.get(task_id)
         effort_remaining = EffortRemaining.get(task_id)
-        IO.puts("#{assignment_name} is for task '#{task_name}' which has #{effort_remaining} left")
-
         worker_effort = WorkerEffort.get(worker_id)
 
-        IO.puts("#{worker_name} is going to do #{worker_effort} to task '#{task_name}'.")
+        IO.puts """
+                    Task: '#{task_name}'
+                      Assignment: '#{assignment_name}'
+                      Worker: #{worker_name}
+                      Effort Remaining: #{effort_remaining}
+                      Effort to apply: #{worker_effort}
+                """
 
         effort_left = max(0, effort_remaining - worker_effort)
 
