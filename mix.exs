385970baf75588184a2ebf956255b062ffa15377
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-10-08 21:42:54 +0100

    add ecsx

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Taiichi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ecsx, "~> 0.5"},
       {:phoenix, "~> 1.7.18"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
