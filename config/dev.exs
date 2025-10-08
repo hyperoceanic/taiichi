9f5b6e55bd503689ff86ae3856186067cfa53a2d
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-10-08 21:39:32 +0100

    mix phx.new complete

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,15 @@
 import Config
 
+# Configure your database
+config :taiichi, Taiichi.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "taiichi_dev",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
