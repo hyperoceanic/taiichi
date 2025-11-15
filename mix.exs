0886cc798502333ee42c62ace18c89346aee488f
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-11-15 18:12:36 +0000

    mix ecsx.setup

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule Taiichi.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      listeners: [Phoenix.CodeReloader]
     ]
   end
 
@@ -32,7 +33,8 @@ defmodule Taiichi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.18"},
+      {:ecsx, "~> 0.5"},
+      {:phoenix, "~> 1.8.1"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
